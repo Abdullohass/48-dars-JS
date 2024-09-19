790bf809046010f66af1c2ccf4db0676b922ccba
Abdullohass <abdulloh.s.gk@gmail.com>
2024-09-19 15:02:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// const car = {
+//     nomi: "Malibu 2 Turbo",
+//     yilli: 2023,
+//     rangi: "Black",
+//     matori: "1.5 turbo",
+//     bag: "50 litr",
+//     nomer: "01027SAS",
+//     gaz: false,
+//     owner: {
+//         ism: "Abdulloh",
+//         familya: "Sobirjonov",
+//         yosh: "16 yosh"
+//     }
+// }
+
+// console.log(car);
+// car.nomi = "Malibu 2";
+// car.yilli = 2024
+// car.ruli = "Mers rulli"
+// delete car.rangi;
+
+// console.log(car);
+
+
+const person = {
+    name: null,
+    id: null,
+    password: null,
+}
+
+console.log(person);
+
+person.name = prompt("Ismingizni kiriting");
+person.id = prompt("Idiyingizni kiriting");
+person.password = prompt("Passwordingizni kiriting");
+
+console.log(person);
+// console.log(person);
+// console.log(person);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
